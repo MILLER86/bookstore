34b51685795cccc75b94e5b829d0dac9b2c16137
you@example.com <you@example.com>
2018-04-21 16:29:09 -0500

    introduccion a vuejs

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyC8Sc_ixQ_IH_i0CqnzvDDQP1XxtoUPvg0",
+    authDomain: "bookstore-3fd45.firebaseapp.com",
+    databaseURL: "https://bookstore-3fd45.firebaseio.com",
+    projectId: "bookstore-3fd45",
+    storageBucket: "bookstore-3fd45.appspot.com",
+    messagingSenderId: "417075331426"
+  };
+  firebase.initializeApp(config);
